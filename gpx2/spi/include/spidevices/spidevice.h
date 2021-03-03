266b0245db0a8b9f5b4f6640df370bd3e582fd11
marvin5300 <37504638+marvin5300@users.noreply.github.com>
2021-03-03 23:25:07 +0100

    Fixed missing change of spi mode name.

--- gpx2/spi/include/spidevices/spidevice.h
+++ gpx2/spi/include/spidevices/spidevice.h
@@ -17,7 +17,7 @@ namespace SPI {
 		spiDevice();
 		~spiDevice();
 
-		auto init(std::string busAddress = "/dev/spidev0.0", std::uint32_t speed = 61035, Mode mode = Mode::SPI_MODE_0, uint8_t bits = 8)->bool;
+		auto init(std::string busAddress = "/dev/spidev0.0", std::uint32_t speed = 61035, Mode mode = Mode::spi_mode_0, uint8_t bits = 8)->bool;
 
 		/**
 		* Writes to physical device
