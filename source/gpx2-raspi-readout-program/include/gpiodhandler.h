5ddb5ba7ec8339c7da2df0d9d3da3349cbc86aad
marvin5300 <37504638+marvin5300@users.noreply.github.com>
2021-03-04 03:04:10 +0100

    Added not finished class gpiodhandler which should manage gpio pins using the kernel driver.

---
+++ source/gpx2-raspi-readout-program/include/gpiodhandler.h
@@ -0,0 +1,32 @@
+#ifndef GPIODHANDLER_H
+#define GPIODHANDLER_H
+#include <cstdint>
+#include <vector>
+#include <memory>
+extern "C" {
+#include <gpiod.h>
+}
+enum class PinBias : std::uint8_t {
+	OpenDrain = 0x01,
+	OpenSource = 0x02,
+	ActiveLow = 0x04,
+	PullDown = 0x10,
+	PullUp = 0x20
+};
+class GpiodHandler {
+public:
+	GpiodHandler();
+	~GpiodHandler();
+	auto init(const std::vector<unsigned int>& gpioPins)->bool;
+private:
+	auto setInput(unsigned int gpio, PinBias bias = PinBias::PullDown);
+	auto setOutput(unsigned int gpio, bool initState = false);
+
+	const char* consumer = "gpiodhandler";
+	//std::shared_ptr<gpiod_chip> chip{ nullptr };
+	gpiod_chip* chip{ nullptr };
+	std::vector<gpiod_line*> lines{};
+
+};
+
+#endif // GPIODHANDLER_H
