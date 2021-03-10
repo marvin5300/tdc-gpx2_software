d6fa00d3894cae461ed438062e7347107c07a8ff
marvin5300 <37504638+marvin5300@users.noreply.github.com>
2021-03-10 01:44:04 +0100

    Fixed deboug output. Fixed inversed if.

--- source/gpx2-raspi-readout-program/src/main.cpp
+++ source/gpx2-raspi-readout-program/src/main.cpp
@@ -7,7 +7,7 @@
 
 constexpr double pico_second = 1e-12;
 static double diff(const SPI::GPX2_TDC::Meas& first, const SPI::GPX2_TDC::Meas& second){
-	if (first.status==SPI::GPX2_TDC::Meas::Invalid || second.status!=SPI::GPX2_TDC::Meas::Invalid || first.refclk_freq != second.refclk_freq || first.refclk_freq==0.){
+	if (first.status==SPI::GPX2_TDC::Meas::Invalid || second.status==SPI::GPX2_TDC::Meas::Invalid || first.refclk_freq != second.refclk_freq || first.refclk_freq==0.){
 		return 0;// std::cout << "measurement not valid" << std::endl;
 	}
 	double refclk_period = 1/first.refclk_freq;
@@ -38,10 +38,14 @@ auto main()->int{
 
 	gpx2.init();
 
+	int verbosity = 0;
+
 	bool status = gpx2.write_config(conf);
 	std::string config = gpx2.read_config();
 	if (status&&(config==conf.str())) {
-		std::cout << "config written..." << std::endl;
+		if (verbosity > 1) {
+			std::cout << "config written..." << std::endl;
+		}
 	}
 	else {
 		std::cout << "tried to write:" << std::endl;
@@ -64,7 +68,9 @@ auto main()->int{
 		gpx2.init_reset();
 		auto event = callback->wait(std::chrono::milliseconds{10000});
 		if (!event) {
-			std::cout << "event was neither rising nor falling. This should not happen. Stopping." << std::endl;
+			if (verbosity > 1) {
+				std::cout << "event was neither rising nor falling. This should not happen. Stopping." << std::endl;
+			}
 			break;
 		}
 		/*if (event.type == gpio::event::Rising){
@@ -74,7 +80,10 @@ auto main()->int{
 			//std::cout << "gpio " << event.pin << " falling edge" << std::endl;
 			std::vector<SPI::GPX2_TDC::Meas> readout = gpx2.read_results();
 			if (readout.size()<4){
-				std::cout << "readout vector has less than 4 channels";
+				if (verbosity > 1) {
+					std::cout << "readout vector has less than 4 channels";
+				}
+				continue;
 			}
 			std::cout << diff(readout[0], readout[1]) << std::endl;
 		}
