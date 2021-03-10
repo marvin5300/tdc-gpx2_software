f415db32031444c55314d176d89f2cf83eec1231
marvin5300 <37504638+marvin5300@users.noreply.github.com>
2021-03-10 01:38:29 +0100

    Fixed bug where valid messages were labeled as invalid in debug output. Removed unneeded debug outputs.

--- source/gpx2-raspi-readout-program/src/main.cpp
+++ source/gpx2-raspi-readout-program/src/main.cpp
@@ -7,8 +7,8 @@
 
 constexpr double pico_second = 1e-12;
 static double diff(const SPI::GPX2_TDC::Meas& first, const SPI::GPX2_TDC::Meas& second){
-	if (first.status!=SPI::GPX2_TDC::Meas::Invalid || second.status!=SPI::GPX2_TDC::Meas::Invalid || first.refclk_freq != second.refclk_freq || first.refclk_freq==0.){
-		std::cout << "measurement not valid" << std::endl;
+	if (first.status==SPI::GPX2_TDC::Meas::Invalid || second.status!=SPI::GPX2_TDC::Meas::Invalid || first.refclk_freq != second.refclk_freq || first.refclk_freq==0.){
+		return 0;// std::cout << "measurement not valid" << std::endl;
 	}
 	double refclk_period = 1/first.refclk_freq;
 	double stop_first = first.lsb_ps*first.stop_result;
@@ -67,16 +67,16 @@ auto main()->int{
 			std::cout << "event was neither rising nor falling. This should not happen. Stopping." << std::endl;
 			break;
 		}
-		if (event.type == gpio::event::Rising){
+		/*if (event.type == gpio::event::Rising){
 			std::cout << "gpio " << event.pin << " rising edge" << std::endl;
-		}
+		}*/
 		if (event.type == gpio::event::Falling){
-			std::cout << "gpio " << event.pin << " falling edge" << std::endl;
+			//std::cout << "gpio " << event.pin << " falling edge" << std::endl;
 			std::vector<SPI::GPX2_TDC::Meas> readout = gpx2.read_results();
 			if (readout.size()<4){
 				std::cout << "readout vector has less than 4 channels";
 			}
-			std::cout << "dt = " << diff(readout[0], readout[1]) << std::endl;
+			std::cout << diff(readout[0], readout[1]) << std::endl;
 		}
 	}
 
